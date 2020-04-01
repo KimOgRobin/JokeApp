ded245f8027c0a4d05e4d7434669067008a05264
Robinlp <Robinlp@users.noreply.github.com>
2020-04-01 10:38:56 +0200

    .

--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.java
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.jokeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingBar = findViewById(R.id.ratingBar);
+    }
+
+
+    public void newJoke(View view){
+
+
+    }
+
+    public void saveJoke(View view){
+
+    }
+
+    public void showJokes(View view){
+
     }
 }
