2becd589b851bdd5dd0c9fd664527f34547b3e0c
Robinlp <Robinlp@users.noreply.github.com>
2020-04-04 22:13:39 +0200

    load a new joke
    load a joke using a new thread
    text stays after config change

--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.java
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.java
@@ -1,26 +1,66 @@
 package com.example.jokeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private RatingBar ratingBar;
+    private Retrofit retrofit;
+    private Thread thread;
+    private TextView jokeText;
+    private JokeService jokeAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jokeText = findViewById(R.id.jokeText);
         ratingBar = findViewById(R.id.ratingBar);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://official-joke-api.appspot.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        jokeAPI = retrofit.create(JokeService.class);
     }
 
 
     public void newJoke(View view){
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Call<Joke> jokeCall = jokeAPI.getJoke();
+                try {
+                    Joke theJoke = jokeCall.execute().body();
+                    String jokeOnScreen = theJoke.toString();
+                    jokeText.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            jokeText.setText(jokeOnScreen);
+                        }
+                    });
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        thread.start();
 
     }
 
@@ -30,5 +70,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void showJokes(View view){
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //The text in the jokeText view is saved so it can be used after configuration changes, because android does not save the state of textView
+        outState.putString("joke", String.valueOf(jokeText.getText()));
+        super.onSaveInstanceState(outState);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        //Here the joke is loaded and put into the jokeText View so it can be seen after configuration changes
+        jokeText.setText((CharSequence) savedInstanceState.get("joke"));
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
