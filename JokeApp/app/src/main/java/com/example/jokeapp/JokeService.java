cfd23563fd11511b036146f382d66cdc1265e268
Kim Christensen <32239917+Snowball44@users.noreply.github.com>
2020-04-22 08:52:46 +0200

    fix revert error

--- JokeApp/app/src/main/java/com/example/jokeapp/JokeService.java
+++ JokeApp/app/src/main/java/com/example/jokeapp/JokeService.java
@@ -2,9 +2,12 @@ package com.example.jokeapp;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-
+import java.util.List;
 interface JokeService {
 
     @GET("random_joke")
     public Call<Joke> getJoke();
+
+    @GET("random_ten")
+    public Call<List<Joke>> getTenJokes();
 }
